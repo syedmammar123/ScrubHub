f5a8a7a85bd2e4b1f9c4b6b7804fd5df927a801d
hassan shahid <hassanshahid234@gmail.com>
2025-01-13 15:59:47 +0500

    get random question functionality donee

---
+++ util/getRandomItem.js
@@ -0,0 +1,27 @@
+export const types = [
+    'firstLineTreatment',
+    'flowChart',
+    'lab',
+    'matchTheMicrobe',
+    'medicationUse',
+    'quickDiagnosis',
+    'scrabble',
+    'shortFacts',
+    'testToOrder',
+  ];
+
+export const getRandomArray = (array) => {
+    let arr = []
+    for (let i = 0; i < types.length; i++) {
+      arr.push({
+        topic: array[Math.floor(Math.random() * array.length)],
+        type: types[i],
+        subTopic: null,
+      });
+    }
+    return arr;
+};
+
+export const getRandomItem = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+}
