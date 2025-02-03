1895fc39268353dd792ea7603086d990b7317422
Syed Muhammad Ammar <syedmammar123@gmail.com>
2025-02-03 15:24:54 +0500

    formatted and merge!

--- app/details.jsx
+++ app/details.jsx
@@ -40,7 +40,7 @@ export default function App() {
   const { getCurrentType } = useQuesStore((state) => state);
 
   const router = useRouter();
-  
+
   const handlePress = (system) => {
     console.log("TYPE", getCurrentType());
 
--- app/topics.jsx
+++ app/topics.jsx
@@ -66,7 +66,7 @@ export default function Topics() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  
+
   const handlePress = async (topic) => {
     console.log(system);
     if (getCurrentType() === "review") {
@@ -81,7 +81,7 @@ export default function Topics() {
         } else {
           const lengthOfQuestions = await fetchReviewQuestions(
             system.toLowerCase(),
-            topic
+            topic,
           );
           console.log("LENGTH GIVEN AT", lengthOfQuestions);
 
@@ -128,7 +128,7 @@ export default function Topics() {
     const topicsCollectionRef = doc(
       db,
       "Topics",
-      system.toLowerCase().replace(/\s+/g, "")
+      system.toLowerCase().replace(/\s+/g, ""),
     );
     try {
       const topics = await getDoc(topicsCollectionRef);
